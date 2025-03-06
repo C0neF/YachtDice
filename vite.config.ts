734a93a1171471fc28cb4f99c5fbbd33793edb4c
ConeF <conef@qq.com>
2025-03-06 22:30:36 +0800

    change output directory

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build' // 将输出目录从 'dist' 改为 'build'
+  }
 })
