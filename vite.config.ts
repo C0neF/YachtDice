bcf3f08f9c1037466cc9d138b84e723c575b8804
ConeF <conef@qq.com>
2025-03-07 14:51:26 +0800

    dist

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'build' // 将输出目录从 'dist' 改为 'build'
+    outDir: 'dist' // 将输出目录从 'dist' 改为 'build'
   }
 })
